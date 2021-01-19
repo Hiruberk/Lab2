34c759d9b1f00adcbd138f6bd13eb3cd21f93d75
Nate <nsdavis@mtu.edu>
2021-01-19 15:08:46 -0500

    intial commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Circus Room Detail Generator!");
+            bool again = true;
+
+            do
+            {
+                Console.WriteLine("Enter Length (ft): ");
+                double L = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Width (ft): ");
+                double w = double.Parse(Console.ReadLine());
+                double area = L * w;
+                double perimeter = 2 * L + 2 * w;
+                Console.WriteLine("Area: " + area);
+                Console.WriteLine("Perimeter: " + perimeter);
+                again = false;
+            } while (again == true);
+        }
+    }
+}
